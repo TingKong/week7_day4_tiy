f953c42f079a1b93a0d9d108f541fad683c6d34a
TingKong <kongsau@gmail.com>
2016-11-14 15:12:25 -0500

    Create third table and create page

--- StudentApp/StudentApp/Controllers/HomeController.cs
+++ StudentApp/StudentApp/Controllers/HomeController.cs
@@ -8,10 +8,46 @@ namespace StudentApp.Controllers
 {
     public class HomeController : Controller
     {
+        SchoolAppEntities1 db = new SchoolAppEntities1();
+
         // GET: Home
         public ActionResult Index()
         {
+            var Schoolinfo = from stud_detail in db.Students
+                           join stuNcours in db.StuCourses on stud_detail.Id equals stuNcours.StudentID
+                           join cour_detail in db.Courses on stuNcours.CourseID equals cour_detail.ID
+                           select new StudentApp.Models.School
+                           {
+                               SI = stud_detail,
+                               CI = cour_detail
+
+                           };
+
+            return View(Schoolinfo);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.StudentID = new SelectList(db.Students, "Id", "StudentName");
+            ViewBag.CourseID = new SelectList(db.Courses, "ID", "CourseName");
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudentID,CourseID")] StuCourse schoolinfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StuCourses.Add(schoolinfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StudentID = new SelectList(db.Students, "Id", "StudentName");
+            ViewBag.CourseID = new SelectList(db.Courses, "ID", "CourseName");
+            return View(schoolinfo);
+        }
+
     }
 }
---
+++ StudentApp/StudentApp/Models/School.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace StudentApp.Models
+{
+    public class School
+    {
+
+        [DisplayName("Student")]
+        public Student SI { get; set; }
+
+        [DisplayName("Course")]
+        public Cours CI { get; set; }
+
+
+
+        SchoolAppEntities1 db = new SchoolAppEntities1();
+
+    }
+}
