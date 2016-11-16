e9a9687d37d955002feb8366ea4a2d11ecc93e56
TingKong <kongsau@gmail.com>
2016-11-16 16:48:39 -0500

    create partial class

---
+++ StudentApp/StudentApp/Models/Partialclass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentApp
+{
+  
+        [MetadataType(typeof(StudentMetaData))]
+        public partial class Student
+        {
+
+        }
+        
+            public class StudentMetaData
+        {
+            [DisplayName("Student ID")]
+            public int Id { get; set; }
+            [DisplayName("Student Name")]
+            public string StudentName { get; set; }
+        }
+
+    [MetadataType(typeof(CoursMetaData))]
+    public partial class Cours
+    {
+
+    }
+    public class CoursMetaData
+    {
+        [DisplayName("Courses ID")]
+        public int ID { get; set; }
+        [DisplayName("Course Names")]
+        public string CourseName { get; set; }
+        [DisplayName("Course Descriptions")]
+        public string CourseDesc { get; set; }
+    }
+
+}
